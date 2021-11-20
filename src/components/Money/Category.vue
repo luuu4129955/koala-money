52fd210c0eb6f3d5574644b1be674d6d7a4b9429
crystalpeng <crystalpeng1998@gmail.com>
2021-11-20 14:40:54 +0800

    ts 写了一个Category组件

--- src/components/Money/Category.vue
+++ src/components/Money/Category.vue
@@ -1,14 +1,46 @@
 <template>
-  <div class="category">
-    <div class="pay">支出</div>
-    <div class="income">收入</div>
+  <div>
+    <ul class="category">
+      <li :class="category==='-'&&'selected'"
+          @click="selectCategory('-')">支出
+      </li>
+      <li :class="category==='+'&&'selected'"
+          @click="selectCategory('+')">收入
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Category'
-};
+import Vue from "vue";
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Category extends Vue {
+  category = '-'//'-'表示支出，'+'表示收入
+  selectCategory(category:string ) {
+    if (category !== '-' && category !== '+') {
+      throw new Error('category is unknown');
+    }
+    this.category = category;
+  }
+}
+// export default {
+//   name: 'Category',
+//   data() {
+//     return {
+//       category: '-'//'-'表示支出，'+'表示收入
+//     };
+//   },
+//   methods: {
+//     selectCategory(category){
+//       if (category !== '-' && category !== '+') {
+//         throw new Error('category is unknown');
+//       }
+//       this.category = category;
+//     }
+//   }
+// };
 </script>
 
 <style lang="scss" scoped>
@@ -17,13 +49,24 @@ export default {
   flex-direction: row;
   justify-content: space-around;
 
-  .pay, .income {
+  li {
     width: 100%;
     padding: 12px 0;
     text-align: center;
     background-color: #a2dd9e;
     color: #fff;
     font-size: 20px;
+    position: relative;
+
+    &.selected::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #120142;
+    }
   }
 }
 </style>
