9ff212e392516372b944fc855e83cbac64cbf744
crystalpeng <crystalpeng1998@gmail.com>
2021-12-08 22:57:37 +0800

    Tags v-for创建一组带icon的默认tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -2,73 +2,9 @@
   <div class="tags">
     <div class="tags-list">
       <ul>
-        <li>
-          <Icon name="foods"></Icon>
-          <span>餐饮</span>
-        </li>
-        <li>
-          <Icon name="athletics"></Icon>
-          <span>运动</span>
-        </li>
-        <li>
-          <Icon name="clothes"></Icon>
-          <span>服饰</span>
-        </li>
-        <li>
-          <Icon name="digital"></Icon>
-          <span>数码</span>
-        </li>
-        <li>
-          <Icon name="donate"></Icon>
-          <span>捐赠</span>
-        </li>
-        <li>
-          <Icon name="education"></Icon>
-          <span>教育</span>
-        </li>
-        <li>
-          <Icon name="entertainment"></Icon>
-          <span>娱乐</span>
-        </li>
-        <li>
-          <Icon name="games"></Icon>
-          <span>游戏</span>
-        </li>
-        <li>
-          <Icon name="home"></Icon>
-          <span>家居</span>
-        </li>
-        <li>
-          <Icon name="makeup"></Icon>
-          <span>美妆</span>
-        </li>
-        <li>
-          <Icon name="medical"></Icon>
-          <span>医疗</span>
-        </li>
-        <li>
-          <Icon name="objects"></Icon>
-          <span>日用品</span>
-        </li>
-        <li>
-          <Icon name="pets"></Icon>
-          <span>宠物</span>
-        </li>
-        <li>
-          <Icon name="shopping"></Icon>
-          <span>购物</span>
-        </li>
-        <li>
-          <Icon name="social"></Icon>
-          <span>社交</span>
-        </li>
-        <li>
-          <Icon name="traffic"></Icon>
-          <span>交通</span>
-        </li>
-        <li>
-          <Icon name="others"></Icon>
-          <span>其他</span>
+        <li v-for="tag in defaultTags" :key="tag.id">
+          <Icon :name="tag.id"></Icon>
+          <span>{{ tag.name }}</span>
         </li>
         <li @click="createTag">
           <Icon name="add"></Icon>
@@ -83,17 +19,19 @@
 import {Component} from 'vue-property-decorator';
 import {mixins} from 'vue-class-component';
 import TagHelper from '@/mixins/TagHelper';
+import defaultTags from '@/lib/constant';
 
 
 @Component({
   computed: {
-    tagList() {
+    createTagList() {
       return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
+  defaultTags = defaultTags;
 
   created() {
     this.$store.commit('fetchTags');
