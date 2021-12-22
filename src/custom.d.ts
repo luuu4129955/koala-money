f19fdfa9971069df8f054a5443fcfd52fd043a52
crystalpeng <crystalpeng1998@gmail.com>
2021-12-22 22:38:14 +0800

    删除unused声明

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -10,14 +10,3 @@ type Tag = {
   id: string,
   name: string
 }
-type Store= {
-  tagList: Tag[];
-  addTag: (name: string) => void;
-  findTag: (id: string) => Tag;
-  removeTag: (id: string) => boolean;
-  updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
-  recordList: RecordItem[];
-  createRecord: (record: RecordItem) => void
-};
-
-
